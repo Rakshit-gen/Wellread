0718f5136ccca73b202c4d714a50c79c035a2463
Rakshit Sisodiya <64471316+Rakshit-gen@users.noreply.github.com>
2024-05-10 20:54:12 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,7 +38,7 @@ function Header() {
 
 
   return (
-    <header className='py-3 shadow bg-blue-600'>
+    <header className='py-3 shadow bg-blue-700'>
       <Container>
         <nav className='flex'>
           <div className='mr-4'>
